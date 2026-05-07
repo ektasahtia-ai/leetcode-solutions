6777636f342a6de1219a374ca39d91f7da6923f7
ektasahtia-ai <ektasahtia@gmail.com>
2026-05-07 20:06:40 +0530

    Time: 9 ms (69.20%) | Memory: 14.1 MB (33.52%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for(int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while(left < right) {
+                int current_sum = nums[i] + nums[left] + nums[right];
+
+                if(abs(target - current_sum) < abs(target - closest)) {
+                    closest = current_sum;
+                }
+
+                if(current_sum < target) {
+                    left++;
+                }
+                else if(current_sum > target) {
+                    right--;
+                }
+                else {
+                    return current_sum;
+                }
+            }
+        }
+
+        return closest;
+    }
+};
