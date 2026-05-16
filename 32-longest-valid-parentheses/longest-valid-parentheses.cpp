a3f72d2882078a9029e7c082e73b1c200d70967b
ektasahtia-ai <ektasahtia@gmail.com>
2026-05-16 22:03:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (72.07%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1); // base index
+        int maxi = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(s[i] == '(') {
+                st.push(i);
+            }
+            else {
+                st.pop();
+
+                if(st.empty()) {
+                    st.push(i);
+                }
+                else {
+                    maxi = max(maxi, i - st.top());
+                }
+            }
+        }
+
+        return maxi;
+    }
+};
