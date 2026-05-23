7b52fd4fa9e44ec30d0a55839457d58ea85811a6
ektasahtia-ai <ektasahtia@gmail.com>
2026-05-23 23:46:42 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (46.75%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(vector<int>& nums, vector<int>& temp, vector<bool>& used) {
+        // If permutation is complete
+        if (temp.size() == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Skip already used elements
+            if (used[i]) continue;
+
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+
+            used[i] = true;
+            temp.push_back(nums[i]);
+
+            backtrack(nums, temp, used);
+
+            // Backtrack
+            temp.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> temp;
+        vector<bool> used(nums.size(), false);
+
+        backtrack(nums, temp, used);
+
+        return ans;
+    }
+};
