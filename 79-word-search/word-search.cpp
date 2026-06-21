4d062bb0358c96a13d90a112b203a07a4e5a2f9c
ektasahtia-ai <ektasahtia@gmail.com>
2026-06-21 22:31:18 +0530

    Time: 191 ms (81.76%) | Memory: 10.5 MB (65.26%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board, string& word, int r, int c, int idx) {
+        if (idx == word.size())
+            return true;
+
+        int m = board.size();
+        int n = board[0].size();
+
+        if (r < 0 || r >= m || c < 0 || c >= n ||
+            board[r][c] != word[idx])
+            return false;
+
+        char temp = board[r][c];
+        board[r][c] = '#';  // mark visited
+
+        bool found = dfs(board, word, r + 1, c, idx + 1) ||
+                     dfs(board, word, r - 1, c, idx + 1) ||
+                     dfs(board, word, r, c + 1, idx + 1) ||
+                     dfs(board, word, r, c - 1, idx + 1);
+
+        board[r][c] = temp; // backtrack
+
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
